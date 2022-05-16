600d6983bb48041d67b917f4ace8f06a72d52b4d
Matt Andreko <mandreko@gmail.com>
2022-05-16 08:35:33 -0400

    Removed python output

--- python/gemini/__init__.py
+++
@@ -1,6 +0,0 @@
-from .public_client import PublicClient
-from .private_client import PrivateClient
-from .basewebsocket import BaseWebSocket
-from .marketdataws import MarketDataWS
-from .ordereventsws import OrderEventsWS
-from .order_book import GeminiOrderBook
